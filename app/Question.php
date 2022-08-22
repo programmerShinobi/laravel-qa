0fbd56d0c34643f9c474ffb851786949abaff4af
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-08-23 04:38:14 +0700

    Prepupulate users and questions tables using model factories dan seeds

--- app/Question.php
+++ app/Question.php
@@ -12,4 +12,10 @@ class Question extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -31,11 +31,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
-
-    public function setTitleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
-
 }
