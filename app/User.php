521964b4d59d1287377c21b7d2ed6b8ceeb27228
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-08-23 20:45:56 +0700

    Add author info and question creation date in question item

--- app/User.php
+++ app/User.php
@@ -31,4 +31,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+
+    public function getCreatedDateAttribute()
+    {
+        // return $this->created_at->diffForHumans();
+        return'#';
+    }
 }
