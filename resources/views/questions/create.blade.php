10d0ed20452ed0000f6b099808058e0da8cb259b
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-08-24 11:55:37 +0700

    Implement from validation and store the question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -21,7 +21,7 @@
                         @csrf
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+                            <input type="text" name="title" value="{{ old('title') }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
 
                             @if ($errors->has('title'))
                                 <div class="invalid-feedback">
@@ -31,7 +31,7 @@
                         </div>
                         <div class="form-group">
                             <label for="question-body">Explain your question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body') }}</textarea>
 
                             @if ($errors->has('body'))
                                 <div class="invalid-feedback">
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -17,19 +17,21 @@
                 </div>
 
                 <div class="card-body">
-                   @foreach ($questions as $question)
-                       <div class="media">
-                        <div class="d-flex flex-column counters">
-                            <div class="vote">
-                                <strong>{{ $question->votes }}</strong> {{ str_plural('vote',$question->votes) }}
+                    @include('layouts._messages')
+
+                    @foreach ($questions as $question)
+                        <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ str_plural('vote',$question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer',$question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views ." ". str_plural('view',$question->views) }}
+                                </div>
                             </div>
-                            <div class="status {{ $question->status }}">
-                                <strong>{{ $question->answers }}</strong> {{ str_plural('answer',$question->answers) }}
-                            </div>
-                            <div class="view">
-                                {{ $question->views ." ". str_plural('view',$question->views) }}
-                            </div>
-                        </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <p class="lead">
@@ -39,8 +41,8 @@
                                 </p>
                                 {{ str_limit($question->body, 250) }}
                             </div>
-                       </div>
-                       <hr>
+                        </div>
+                        <hr>
                    @endforeach
 
                    <div class="mx-auto">
