f9364d06323a47385dab8f94cd99b1206e692bf3
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-08-24 21:32:57 +0700

    Impement authorization using Gates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Question;
+
+
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
@@ -66,6 +70,9 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        if (Gate::denies('update-question', $question)) {
+            abort(403, "Access denied");
+        }
         return view('questions.edit', compact('question'));
     }
 
@@ -78,6 +85,10 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if (Gate::denies('update-question', $question)) {
+            abort(403, "Access denied");
+        }
+
         $question->update($request->only('title', 'body'));
 
         return redirect('/questions')->with('success', "Your question has been updated.");
@@ -91,6 +102,10 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        if (Gate::denies('delete-question', $question)) {
+            abort(403, "Access denied");
+        }
+
         $question->delete();
 
         return redirect('/questions')->with('success', " Your question has been deleted.");
