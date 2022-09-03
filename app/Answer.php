5fda0a568b6cf8f12184ffd84427ac8569fcf526
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-09-03 11:32:13 +0700

    display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -29,4 +29,9 @@ class Answer extends Model
             $answer->question->save();
         });
     }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
