09993410c7ea200c3d5ed9b4bc2b1761f69af37a
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-09-22 14:34:27 +0700

    Update best_answer_id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -31,12 +31,7 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if ($question->best_answer_id === $answer->id){
-                $question->best_answer_id = NULL;
-                $question->save();
-            }
+            $answer->question->decrement('answers_count');
         });
     }
 
